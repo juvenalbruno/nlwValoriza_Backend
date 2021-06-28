de0b22cdac385f761bc4758c41b38d7da44e989c
juvenalbruno <bruno.andrade178@hotmail.com>
2021-06-28 15:48:15 -0300

    Aula_04

---
+++ src/services/AutenticateUserService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from 'jsonwebtoken';
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const userRepositories = getCustomRepository(UsersRepositories);
+
+        const user = await userRepositories.findOne({ email });
+        
+        if (!user)
+            throw new Error("Email/Password incorrect!");
+        
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch)
+            throw new Error("Email/Password incorrect!");
+        
+        const token = sign(
+            {
+                email: user.email
+            },
+            "2034f6e32958647fdff75d265b455ebf",
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        )
+
+        return token;
+    }
+};
+
+export { AuthenticateUserService };
