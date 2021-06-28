5548e1d8b0d3f525f0f486268854f36e524663c0
juvenalbruno <bruno.andrade178@hotmail.com>
2021-06-28 11:45:00 -0300

    Aula_03

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import 'reflect-metadata';
+import "express-async-errors";
 import './database';
 
-import { router } from './routes';
+import express, { NextFunction, Request, Response } from 'express';
 
-import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error)
+        return res.status(400).json({ error: err.message });
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
 
 app.listen(3000, () => console.log(">>> Server is running!"))
